058f9504df1603cc3c57c3a3e7dba9164e5ef8cb
Luis Designs <luislozano120xd@gmail.com>
2021-12-23 21:35:50 -0500

    ya lo comente mis rutas del archivo api.php

--- routes/api.php
+++ routes/api.php
@@ -56,32 +56,44 @@ Route::get('/depas', [cargosController::class, 'getDepartamentoEmpresa']);
 Route::get('/depas/areas/{idDepa}', [cargosController::class, 'getAreasByDepartamento']);
 Route::get('/depas/areas/cargos/{idArea}', [cargosController::class, 'getCargos']);
 
-
+// Nos trae todos los proveedores que
+// tienen de estado 1
 Route::get('/dataProviders', function () {
     $data = DB::select('exec listProviders');
     return response()->json($data);
 });
 
+// Se utiliza para actualizar un
+// proveedor 
 Route::get('/provider/{id}', function ($id) {
     $data = Proveedor::find($id)->first();
     return response()->json($data);
 });
-
+ 
+// Nos trae todos los ciudadanos que
+// no son ni empleados ni
+// proveedores
 Route::get('/dataProvidersNew', function () {
     $data = DB::select('exec listProvidersNew');
     return response()->json($data);
 });
 
+// Trae todas las categorias de los
+// productos
 Route::get('/dataCategories', function () {
     $data = DB::select('exec spListCategories');
     return response()->json($data);
 });
 
+// Lista los productos por las distintas
+// categorias
 Route::get('/dataProducts/{id}', function ($id) {
     $data = DB::select('exec spListProducts ' . $id);
     return response()->json($data);
 });
 
+// Está pensado para dar mantenimiento
+// a la tabla productos
 Route::get('/products', function () {
     return DB::table('productos')->get();
 });
